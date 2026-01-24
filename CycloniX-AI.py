2a3f40021dbf5ca4342a02612f933152d03190d0
Dmitry Moiseev <149093616+Create2702@users.noreply.github.com>
2026-01-24 12:53:48 +0400

    Add files via upload
    A slightly more modified version of the neural network

--- CycloniX-AI.py
+++ CycloniX-AI.py
@@ -1,10 +1,12 @@
-inputs = [float(input('Ilnur: ')), float(input('Kirill: ')), float(input('Roma: '))]
-print('-------')
-targets = [float(input('Ilnur: ')), float(input('Kirill: ')), float(input('Roma: '))]
+print('--- inputs ---')
+inputs = [float(input('Ilnur: ')), float(input('Kirill: ')), float(input('Rome: '))]
+print('--- targets ---')
+targets = [float(input('Ilnur: ')), float(input('Kirill: ')), float(input('Rome: '))]
+epochs = int(input('Enter a quantity of epochs: '))
 
-weights = [[0.1, 0.2, 0.3],
-           [0.3, 0.2, 0.1],
-           [0.0, 0.1, 0.3]]
+weights = [[0.1, 0.0, 0.9],
+           [0.7, 0.5, 0.2],
+           [0.4, 0.1, 0.0]]
 
 def dot(vect_a, vect_b):
     assert(len(vect_a) == len(vect_b))
@@ -13,43 +15,48 @@ def dot(vect_a, vect_b):
         output += vect_a[i] * vect_b[i]
     return output
 
-def matrix(vect, matrix):
+def matrix_mul(vect, matrix):
     assert(len(vect) == len(matrix))
     output = [0, 0, 0]
-    for i in range(len(matrix)):
+    for i in range(len(vect)):
         output[i] = dot(vect, matrix[i])
     return output
 
-def ele_mul(const, delta):
-    weights_delta = [[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]]
-    for i in range(len(weights_delta)):
-        for j in range(len(weights_delta)):
-            weights_delta[j][i] = const[i] * delta[j]
-    return weights_delta
+def find_error(result_vect, target_vect):
+    assert(len(result_vect) == len(target_vect))
+    lose = [0, 0, 0]
+    for i in range(len(result_vect)):
+        lose[i] = (result_vect[i] - target_vect[i]) ** 2
+    return lose
 
-def forward(input_data, weight_data, target_data):
-    for i in range(10000):
-        final_weights = weight_data
-        assert(len(input_data) == len(weight_data))
-        pred = matrix(input_data, weight_data)
-        error = [0, 0, 0]
-        delta = [0, 0, 0]
-        for ix in range(len(input_data)):
-            error[ix] = (pred[ix] - target_data[ix]) ** 2
-            delta[ix] = pred[ix] - target_data[ix]
-        weights_delta = ele_mul(input_data, delta)
-        for ir in range(len(weights_delta)):
-            for j in range(len(weights_delta)):
-                final_weights[ir][j] -= (weights_delta[ir][j]) * 0.01
-    return final_weights
+def find_delta(result_vect, target_vect):
+    assert(len(result_vect) == len(target_vect))
+    delta = [0, 0, 0]
+    for i in range(len(result_vect)):
+        delta[i] = result_vect[i] - target_vect[i]
+    return delta
 
-def final(final_weights, final_inputs):
-    pred = matrix(final_inputs, final_weights)
-    return pred
+def correct_weight(input_data, delta, current_weights):
+    assert(len(input_data) == len(delta))
+    alpha = 0.01
+    new_weights = [[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]
+    for i in range(len(new_weights)):
+        for j in range(len(new_weights)):
+            new_weights[j][i] = input_data[i] * delta[j]
+    for string in range(len(new_weights)):
+        for ele in range(len(new_weights)):
+            current_weights[ele][string] -= new_weights[ele][string] * alpha
+    return current_weights
 
-weight = forward(inputs, weights, targets)
-pred = final(weight, inputs)
-print(pred)
+def start(input_data, weight_data, target_data):
+    for learn in range(epochs):
+        pred = matrix_mul(input_data, weight_data)
+        delta = find_delta(pred, target_data)
+        weight_data = correct_weight(input_data, delta, weight_data)
+    print('Done!')
+    return weight_data
 
+weights = start(inputs, weights, targets)
+print(matrix_mul(inputs, weights))
